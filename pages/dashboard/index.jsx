86088edda2db5e225639bbe41438337b88aa3743
Sinchan Shetty <sinchannshetty@gmail.com>
2023-11-03 19:35:04 +0530

    dashboard layout

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
+import styles from "styles/components/dashboard/dashboard.module.scss";
 import { useAuth } from "@/lib/hooks/AuthHook";
 function Dashboard() {
     return (
         <>
             <useAuth>
-                <h1>DASHBOARD</h1>
+                <section className={styles.navbar} >
+                    <h1>travelEase</h1>
+                    <div className={styles.flexele}> 
+                         <h3>Home</h3>
+                         <h3>Cart</h3>
+                         <h3>Contact us</h3>
+                    </div>
+                </section>
             </useAuth>
         </>
     );
