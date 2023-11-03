b0f1da3796fa4585900bf7a34f374db9cb91e844
Sinchan Shetty <sinchannshetty@gmail.com>
2023-11-04 01:21:31 +0530

    ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }) {
   if (dashBoard) {
     return (
       <>
-        <Nav />
+        {/* <Nav /> */}
         <Component {...pageProps} />
         {/* <Sidebar /> */}
         <Footer />
